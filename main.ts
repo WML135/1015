72fc54dcfdfc6f5ba5fd7a77c2afff4656a21e9a
WML135 <92579915+WML135@users.noreply.github.com>
2021-10-15 18:52:48 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,10 @@
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(262, music.beat(BeatFraction.Double))
+basic.forever(function () {
+	
+})
